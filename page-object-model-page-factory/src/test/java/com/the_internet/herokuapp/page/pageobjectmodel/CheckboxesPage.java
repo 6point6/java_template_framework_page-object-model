79119850caf760957a05e487a18e53db9a7fe8d1
Deen <deen.animiokhali@6point6.com>
2021-07-25 16:19:12 +0100

    Page Object Model template

---
+++ page-object-model-page-factory/src/test/java/com/the_internet/herokuapp/page/pageobjectmodel/CheckboxesPage.java
@@ -0,0 +1,51 @@
+package com.the_internet.herokuapp.page.pageobjectmodel;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CheckboxesPage extends BasePage {
+
+    private static final By pageTitleLocator = By.tagName("h3");
+    private static final By checkboxesFormLocator = By.id("checkboxes");
+    private static final By checkboxLocator = By.tagName("input");
+    private static final By checkboxLabelLocator = By.xpath("text()");
+
+    private WebElement getPageTitle() {
+        return driver.findElement(pageTitleLocator);
+    }
+
+    private WebElement getCheckboxesForm() {
+        return driver.findElement(checkboxesFormLocator);
+    }
+
+    private List<WebElement> getAllCheckboxes() {
+        return getCheckboxesForm().findElements(checkboxLocator);
+    }
+
+    @Override
+    public String getPageTitleText() {
+        return getPageTitle().getText();
+    }
+
+    public int getNumCheckboxes() {
+        return getAllCheckboxes().size();
+    }
+
+    public List<String> getAllCheckboxLabels() {
+        String[] checkboxLabels = getCheckboxesForm().getText().split("\n");
+        return Arrays.asList(checkboxLabels.clone());
+    }
+
+    public boolean getCheckboxCheckedState(int index) {
+        List<WebElement> checkboxes = getAllCheckboxes();
+        return checkboxes.get(index).getAttribute("checked") != null;
+    }
+
+    public void clickOnCheckbox(int index) {
+        List<WebElement> checkboxes = getAllCheckboxes();
+        checkboxes.get(index).click();
+    }
+}
